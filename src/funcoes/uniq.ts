93852f945e9ef015ab1d08970508a8455f485aa2
Jonathan Chu <jmarvinchu@gmail.com>
2022-03-04 23:22:28 -0300

    implementado uniq

--- src/funcoes/uniq.ts
+++ src/funcoes/uniq.ts
@@ -7,6 +7,25 @@
  * 
  * @returns somente os itens definidos.
  */
+
 export const uniq = <T>(args: T[]): T[] => {
-  return null;
+  let arrResultado: T[] = []
+  for(let i = 0; i < args.length; i++){
+    if(counter(args[i],arrResultado)===0){
+      arrResultado.push(args[i])
+    } else{
+      continue
+    }
+  }
+
+  return arrResultado;
 };
+
+
+const counter = (eleVerificado: unknown, arrayVerificado: unknown[]) :number => {
+  let contador: number = 0
+  arrayVerificado.forEach(eleEach => {
+    if(eleVerificado === eleEach) contador++
+  });
+  return contador
+}
